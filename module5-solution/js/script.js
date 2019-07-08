94839fe037b233e9eff2c815c9a224733ccc81a5
a4asadlv <51232383+a4asadlv@users.noreply.github.com>
2019-07-08 18:26:41 +0500

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -1,6 +1,4 @@
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
-  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+$(function () { 
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 768) {
